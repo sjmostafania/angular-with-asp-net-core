0a76da155c39c326b0857e797127bb72af3872d4
jalal mostafania <s.j.mostafania@gmail.com>
2019-08-26 19:06:10 +0430

    first commit

---
+++ UserManager/src/app/shared/user.service.ts
@@ -0,0 +1,54 @@
+import { Injectable } from '@angular/core';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { HttpClient, HttpHeaders } from '@Angular/common/http';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
+  readonly EndPoint = "http://localhost:55889/api";
+
+  formModel = this.fb.group({
+    UserName: ['', Validators.required],
+    Email: ['', Validators.email],
+    FullName: [''],
+    Passwords: this.fb.group({
+      Password: ['', [Validators.required, Validators.minLength(4)]],
+      ConfirmPassword: ['', Validators.required]
+    }, { validator: this.comparePasswords })
+
+  });
+
+  comparePasswords(fb: FormGroup) {
+    let confirmPswrdCtrl = fb.get('ConfirmPassword');
+    //passwordMismatch
+    //confirmPswrdCtrl.errors={passwordMismatch:true}
+    if (confirmPswrdCtrl.errors == null || 'passwordMismatch' in confirmPswrdCtrl.errors) {
+      if (fb.get('Password').value != confirmPswrdCtrl.value)
+        confirmPswrdCtrl.setErrors({ passwordMismatch: true });
+      else
+        confirmPswrdCtrl.setErrors(null);
+    }
+  }
+
+  register() {
+    var body = {
+      UserName: this.formModel.value.UserName,
+      Email: this.formModel.value.Email,
+      FullName: this.formModel.value.FullName,
+      Password: this.formModel.value.Passwords.Password,
+    };
+    return this.http.post(`${this.EndPoint}${'/ApplicationUser/Register'}`, body)
+  }
+
+  login(formData) {
+    return this.http.post(`${this.EndPoint}${'/ApplicationUser/Login'}`, formData)
+  }
+
+  getUserProfile() {
+    return this.http.get(`${this.EndPoint}${'/UserProfile'}`);
+  }
+}
