0e8edca6fbd543bd52d5f71cf3198b89288f957f
figagroup <figa@group>
2019-08-31 15:43:30 +0430

    the navbar has moved in the app.component.html

--- UserManager/src/app/app.component.ts
+++ UserManager/src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from './shared/user.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,20 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'UserManager';
+  userDetails;
+
+  constructor(private router: Router, private service: UserService) { }
+
+  ngOnInit() {
+    this.service.getUserProfile().subscribe(res => {
+      this.userDetails = res;
+    },
+      err => {
+        console.log(err);
+      });
+  }
+  onLogout() {
+    localStorage.removeItem('token');
+    this.router.navigate(['/user/login']);
+  }
 }
--- UserManager/src/app/user/registration/registration.component.ts
+++ UserManager/src/app/user/registration/registration.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from 'src/app/shared/user.service';
 import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
@@ -9,32 +10,35 @@ import { ToastrService } from 'ngx-toastr';
 })
 export class RegistrationComponent implements OnInit {
 
-  constructor(public service: UserService,private toaster:ToastrService) { }
+  constructor(public service: UserService, private toaster: ToastrService, private router: Router) { }
 
   ngOnInit() {
     this.service.formModel.reset();
+
+    if (localStorage.getItem('token') != null) {
+      this.router.navigateByUrl('/home');
+    }
   }
 
   onSubmit() {
     this.service.register().subscribe((res: any) => {
       if (res.succeeded) {
         this.service.formModel.reset();
-        this.toaster.success('New User Created','Registration Succesful.');
+        this.toaster.success('New User Created', 'Registration Succesful.');
       } else {
         res.errors.forEach(element => {
           switch (element.code) {
             case 'DuplicateUserName':
-              this.toaster.error('UserName is already taken','Registration faild.')
+              this.toaster.error('UserName is already taken', 'Registration faild.')
               break;
 
             default:
-              this.toaster.error(element.description,'Registration faild.');
+              this.toaster.error(element.description, 'Registration faild.');
               break;
           }
         });
       }
     }, err => {
-
       console.log(err)
     });
   }
